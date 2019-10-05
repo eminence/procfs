bab9cd6d652c43cbe70155e5c056af3bc30bf220
Idanski <idanski1@gmail.com>
2019-10-05 13:05:12 +0300

    Add pressure tests

--- src/pressure.rs
+++ src/pressure.rs
@@ -1,8 +1,9 @@
 //! Pressure stall information retreived from `/proc/pressure/cpu`,
 //! `/proc/pressure/memory` and `/proc/pressure/io`
-//! might not be available on kernels older than 4.20.0
+//! may not be available on kernels older than 4.20.0
 //! For reference: https://lwn.net/Articles/759781/
-use crate::{ProcResult, ProcError};
+
+use crate::{ProcError, ProcResult};
 use std::collections::HashMap;
 
 #[derive(Debug)]
@@ -31,19 +32,17 @@ pub struct IoPressure {
 }
 
 fn get_f32(map: &HashMap<&str, &str>, value: &str) -> ProcResult<f32> {
-    map.get(value)
-       .map_or_else(|| Err(ProcError::Incomplete(None)),
-                    |v| Ok(v.parse::<f32>()
-                            .map_err(|_| ProcError::Incomplete(None))?)
-       )
+    map.get(value).map_or_else(
+        || Err(ProcError::Incomplete(None)),
+        |v| Ok(v.parse::<f32>().map_err(|_| ProcError::Incomplete(None))?),
+    )
 }
 
 fn get_total(map: &HashMap<&str, &str>) -> ProcResult<u64> {
-    map.get("total")
-       .map_or_else(|| Err(ProcError::Incomplete(None)),
-                    |v| Ok(v.parse::<u64>()
-                            .map_err(|_| ProcError::Incomplete(None))?)
-       )
+    map.get("total").map_or_else(
+        || Err(ProcError::Incomplete(None)),
+        |v| Ok(v.parse::<u64>().map_err(|_| ProcError::Incomplete(None))?),
+    )
 }
 
 fn parse_pressure_record(line: &str) -> ProcResult<PressureRecord> {
@@ -69,7 +68,6 @@ fn parse_pressure_record(line: &str) -> ProcResult<PressureRecord> {
         avg300: get_f32(&parsed, "avg300")?,
         total: get_total(&parsed)?,
     })
-
 }
 
 pub fn cpu_pressure() -> ProcResult<CpuPressure> {
@@ -99,7 +97,6 @@ fn get_pressure(pressure_file: &str) -> ProcResult<(PressureRecord, PressureReco
     let mut full = String::new();
     reader.read_line(&mut full)?;
 
-
     Ok((parse_pressure_record(&some)?, parse_pressure_record(&full)?))
 }
 
@@ -109,22 +106,26 @@ pub fn memory_pressure() -> ProcResult<MemoryPressure> {
     Ok(MemoryPressure { some, full })
 }
 
-pub fn io_pressure() -> ProcResult<MemoryPressure> {
+pub fn io_pressure() -> ProcResult<IoPressure> {
     let (some, full) = get_pressure("io")?;
 
-    Ok(MemoryPressure { some, full })
+    Ok(IoPressure { some, full })
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
     use std::f32::EPSILON;
+    use std::path::Path;
+
+    fn valid_percentage(value: f32) -> bool {
+        (value >= 0.00 && value < 100.0)
+    }
 
     #[test]
     fn test_parse_pressure_record() {
         let record =
-            parse_pressure_record("full avg10=2.10 avg60=0.12 avg300=0.00 total=391926")
-            .unwrap();
+            parse_pressure_record("full avg10=2.10 avg60=0.12 avg300=0.00 total=391926").unwrap();
 
         assert!(record.avg10 - 2.10 < EPSILON);
         assert!(record.avg60 - 0.12 < EPSILON);
@@ -134,11 +135,52 @@ mod test {
 
     #[test]
     fn test_parse_pressure_record_errs() {
-        assert!(parse_pressure_record("avg10=2.10 avg60=0.12 avg300=0.00 total=391926")
-                .is_err());
-        assert!(parse_pressure_record("some avg10=2.10 avg300=0.00 total=391926")
-                .is_err());
-        assert!(parse_pressure_record("some avg10=2.10 avg60=0.00 avg300=0.00")
-                .is_err());
+        assert!(parse_pressure_record("avg10=2.10 avg60=0.12 avg300=0.00 total=391926").is_err());
+        assert!(parse_pressure_record("some avg10=2.10 avg300=0.00 total=391926").is_err());
+        assert!(parse_pressure_record("some avg10=2.10 avg60=0.00 avg300=0.00").is_err());
+    }
+
+    #[test]
+    fn test_mem_pressure() {
+        if !Path::new("/proc/pressure/memory").exists() {
+            return;
+        }
+
+        let mem_psi = memory_pressure().unwrap();
+        assert!(valid_percentage(mem_psi.some.avg10));
+        assert!(valid_percentage(mem_psi.some.avg60));
+        assert!(valid_percentage(mem_psi.some.avg300));
+
+        assert!(valid_percentage(mem_psi.full.avg10));
+        assert!(valid_percentage(mem_psi.full.avg60));
+        assert!(valid_percentage(mem_psi.full.avg300));
+    }
+
+    #[test]
+    fn test_io_pressure() {
+        if !Path::new("/proc/pressure/io").exists() {
+            return;
+        }
+
+        let io_psi = io_pressure().unwrap();
+        assert!(valid_percentage(io_psi.some.avg10));
+        assert!(valid_percentage(io_psi.some.avg60));
+        assert!(valid_percentage(io_psi.some.avg300));
+
+        assert!(valid_percentage(io_psi.full.avg10));
+        assert!(valid_percentage(io_psi.full.avg60));
+        assert!(valid_percentage(io_psi.full.avg300));
+    }
+
+    #[test]
+    fn test_cpu_pressure() {
+        if !Path::new("/proc/pressure/cpu").exists() {
+            return;
+        }
+
+        let cpu_psi = cpu_pressure().unwrap();
+        assert!(valid_percentage(cpu_psi.some.avg10));
+        assert!(valid_percentage(cpu_psi.some.avg60));
+        assert!(valid_percentage(cpu_psi.some.avg300));
     }
 }
