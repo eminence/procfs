cd83dec0e5c99150dcf56e827eb462d641ecee75
Andrew Chin <achin@eminence32.net>
2020-07-21 15:27:57 -0400

    Improved parsing of /proc/cpuinfo
    Closes #90

--- src/cpuinfo.rs
+++ src/cpuinfo.rs
@@ -1,6 +1,6 @@
-use crate::ProcResult;
+use crate::{FileWrapper, ProcResult};
 
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Read};
 
 /// Represents the data from `/proc/cpuinfo`.
 ///
@@ -18,21 +18,28 @@ pub struct CpuInfo {
 }
 
 impl CpuInfo {
-    pub fn new() -> ProcResult<CpuInfo> {
-        use std::fs::File;
+    fn from_reader<R: Read>(r: R) -> ProcResult<CpuInfo> {
         use std::io::{BufRead, BufReader};
 
-        let file = File::open("/proc/cpuinfo")?;
-        let reader = BufReader::new(file);
+        let reader = BufReader::new(r);
 
         let mut list = Vec::new();
         let mut map = Some(HashMap::new());
+
+        // the first line of a cpu block must start with "processor"
+        let mut found_first = false;
 
         for line in reader.lines() {
             if let Ok(line) = line {
                 if !line.is_empty() {
                     let mut s = line.split(':');
                     let key = expect!(s.next());
+                    if !found_first && key.trim() == "processor" {
+                        found_first = true;
+                    }
+                    if !found_first {
+                        continue;
+                    }
                     if let Some(value) = s.next() {
                         let key = key.trim().to_owned();
                         let value = value.trim().to_owned();
@@ -41,6 +48,7 @@ impl CpuInfo {
                     }
                 } else if let Some(map) = map.take() {
                     list.push(map);
+                    found_first = false;
                 }
             }
         }
@@ -80,6 +88,11 @@ impl CpuInfo {
             fields: common_map,
             cpus: list,
         })
+    }
+    pub fn new() -> ProcResult<CpuInfo> {
+        let file = FileWrapper::open("/proc/cpuinfo")?;
+
+        CpuInfo::from_reader(file)
     }
 
     /// Get the total number of cpu cores.
@@ -155,5 +168,69 @@ mod tests {
         }
 
         //assert_eq!(info.num_cores(), 8);
+    }
+
+    #[test]
+    fn test_cpuinfo_rpi() {
+        // My rpi system includes some stuff at the end of /proc/cpuinfo that we shouldn't parse
+        let data = r#"processor       : 0
+model name      : ARMv7 Processor rev 4 (v7l)
+BogoMIPS        : 38.40
+Features        : half thumb fastmult vfp edsp neon vfpv3 tls vfpv4 idiva idivt vfpd32 lpae evtstrm crc32
+CPU implementer : 0x41
+CPU architecture: 7
+CPU variant     : 0x0
+CPU part        : 0xd03
+CPU revision    : 4
+
+processor       : 1
+model name      : ARMv7 Processor rev 4 (v7l)
+BogoMIPS        : 38.40
+Features        : half thumb fastmult vfp edsp neon vfpv3 tls vfpv4 idiva idivt vfpd32 lpae evtstrm crc32
+CPU implementer : 0x41
+CPU architecture: 7
+CPU variant     : 0x0
+CPU part        : 0xd03
+CPU revision    : 4
+
+processor       : 2
+model name      : ARMv7 Processor rev 4 (v7l)
+BogoMIPS        : 38.40
+Features        : half thumb fastmult vfp edsp neon vfpv3 tls vfpv4 idiva idivt vfpd32 lpae evtstrm crc32
+CPU implementer : 0x41
+CPU architecture: 7
+CPU variant     : 0x0
+CPU part        : 0xd03
+CPU revision    : 4
+
+processor       : 3
+model name      : ARMv7 Processor rev 4 (v7l)
+BogoMIPS        : 38.40
+Features        : half thumb fastmult vfp edsp neon vfpv3 tls vfpv4 idiva idivt vfpd32 lpae evtstrm crc32
+CPU implementer : 0x41
+CPU architecture: 7
+CPU variant     : 0x0
+CPU part        : 0xd03
+CPU revision    : 4
+
+Hardware        : BCM2835
+Revision        : a020d3
+Serial          : 0000000012345678
+Model           : Raspberry Pi 3 Model B Plus Rev 1.3
+"#;
+
+        let r = std::io::Cursor::new(data.as_bytes());
+
+        let info = CpuInfo::from_reader(r).unwrap();
+        assert_eq!(info.num_cores(), 4);
+        let info = info.get_info(0).unwrap();
+        assert!(info.get("model name").is_some());
+        assert!(info.get("BogoMIPS").is_some());
+        assert!(info.get("Features").is_some());
+        assert!(info.get("CPU implementer").is_some());
+        assert!(info.get("CPU architecture").is_some());
+        assert!(info.get("CPU variant").is_some());
+        assert!(info.get("CPU part").is_some());
+        assert!(info.get("CPU revision").is_some());
     }
 }
