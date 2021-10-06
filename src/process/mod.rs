a3c0ba1d6161fddc19f9c3941dc70725b8602704
tatref <tatref@github.com>
2021-10-07 01:37:10 +0200

    convert /SYSV key to hex

--- src/process/mod.rs
+++ src/process/mod.rs
@@ -466,6 +466,8 @@ pub enum MMapPath {
     Vsyscall,
     /// An anonymous mapping as obtained via mmap(2).
     Anonymous,
+    /// Shared memory segment
+    Vsys(u32),
     /// Some other pseudo-path
     Other(String),
 }
@@ -490,7 +492,7 @@ impl MMapPath {
                 MMapPath::TStack(tid)
             }
             x if x.starts_with('[') && x.ends_with(']') => MMapPath::Other(x[1..x.len() - 1].to_string()),
-            x if x.starts_with("/SYSV") => MMapPath::Other(x[0..x.len()].to_string()),
+            x if x.starts_with("/SYSV") => MMapPath::Vsys(u32::from_str_radix(&x[5..13], 16).unwrap()), // 32bits as hex. /SYSVaabbccdd (deleted)
             x => MMapPath::Path(PathBuf::from(x)),
         })
     }
