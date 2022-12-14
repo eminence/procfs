e76b03e49ce47e1f5e822047c0e7d1419e47873f
tatref <tatref@github.com>
2022-12-14 23:29:19 +0100

    doc

--- examples/pfn.rs
+++ examples/pfn.rs
@@ -1,6 +1,12 @@
 //
-// Print physical memory location for each memory mapping
+// Print physical memory location for each page for each memory mapping
 // This requires CAP_SYS_ADMIN privilege, or root, otherwise physical memory addresses will be zero
+//
+// Vocabulary
+// VA = Virtual Address: memory address from a process point of view
+// VPN = Virtual Page Number: page number of the a Virtual Memory address
+// PA = Physical Address: memory address in physical memory
+// PFN = Page Frame Number: page number of a Physical Address
 //
 
 use procfs::process::MMapPath;
@@ -19,11 +25,11 @@ fn main() {
     let mem_map = process.maps().unwrap();
 
     for memory_map in mem_map {
-        let mem_start = memory_map.address.0;
-        let mem_end = memory_map.address.1;
+        let va_start = memory_map.address.0;
+        let va_end = memory_map.address.1;
 
-        let index_start = (mem_start / page_size) as usize;
-        let index_end = (mem_end / page_size) as usize;
+        let vpn_start = (va_start / page_size) as usize;
+        let vpn_end = (va_end / page_size) as usize;
 
         // can't scan Vsyscall, so skip it
         if memory_map.pathname == MMapPath::Vsyscall {
@@ -32,17 +38,14 @@ fn main() {
 
         println!("Memory mapping {:?}", memory_map);
 
-        for index in index_start..index_end {
-            let virt_mem = index * page_size as usize;
-            let page_info = pagemap.get_info(index).unwrap();
+        for vpn in vpn_start..vpn_end {
+            let va = vpn * page_size as usize;
+            let page_info = pagemap.get_info(vpn).unwrap();
             match page_info {
                 procfs::process::PageInfo::MemoryPage(memory_page) => {
                     let pfn = memory_page.get_page_frame_number();
-                    let phys_addr = pfn * page_size;
-                    println!(
-                        "virt_mem: 0x{:x}, pfn: 0x{:x}, phys_addr: 0x{:x}",
-                        virt_mem, pfn, phys_addr
-                    );
+                    let pa = pfn * page_size;
+                    println!("virt_mem: 0x{:x}, pfn: 0x{:x}, phys_addr: 0x{:x}", va, pfn, pa);
                 }
                 procfs::process::PageInfo::SwapPage(_) => (), // page is in swap
             }
